a8d9948cb2cb0e9f033ed96d405c8fd7b2c5ab86
Xennis <code@xennis.org>
2015-03-22 00:17:33 +0100

    Catch errors

--- page-mt.php
+++ page-mt.php
@@ -6,25 +6,28 @@ get_header();
 		$viewType = get_query_var('mtView');
 		$id = intval(get_query_var('mtId'));
 		if (defined('MT_DIR') && !empty($id)) {
-			switch ($viewType) {
-				case 'bilder/galerie':
-					require_once(MT_DIR . '/public/view/Gallery.php');		
-					$view = new MT_View_Gallery($id);
-					break;
-				case 'bilder/kategorie':
-					require_once(MT_DIR . '/public/view/Category.php');
-					$view = new MT_View_Category($id);
-					break;
-				case 'fotograf':
-					require_once(MT_DIR . '/public/view/Photographer.php');		
-					$view = new MT_View_Photographer($id);
-					break;
-				default:
-					break;
-			}
-			if (isset($view)) {
+			try {
+				switch ($viewType) {
+					case 'bilder/galerie':
+						require_once(MT_DIR . '/public/view/Gallery.php');		
+						$view = new MT_View_Gallery($id);
+						break;
+					case 'bilder/kategorie':
+						require_once(MT_DIR . '/public/view/Category.php');
+						$view = new MT_View_Category($id);
+						break;
+					case 'fotograf':
+						require_once(MT_DIR . '/public/view/Photographer.php');		
+						$view = new MT_View_Photographer($id);
+						break;
+					default:
+						break;
+				}
 				$view->outputContent();				
+			} catch (Exception $e) {
+				echo '<h2>Fehler</h2><p>'.$e->getMessage().'</p>';
 			}
+
 		} else {
 			echo 'Constant MT_DIR is undefined.';
 		}
