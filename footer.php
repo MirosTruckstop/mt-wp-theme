1ecbcb90f55a8703c6f7124395441b924a18c18b
Xennis <code@xennis.org>
2015-03-21 21:21:25 +0100

    Start option to modify the title

--- footer.php
+++ footer.php
@@ -1,16 +1,14 @@
 	<footer class="site-footer">
 		<div id="copyright">&copy; 2006-<?php echo date('Y'); ?> by Michael &quot;MiRo&quot; R.</div>
-		<?php
-		$args = array(
+		<?php wp_nav_menu(array(
 			'theme_location' => 'footer'
-		);
-		?>
-		<?php wp_nav_menu( $args ); ?>		
+		)); ?>		
 	</footer>
 
 	</div><!-- End content_container -->
 </div><!-- End Container -->
 
 <?php wp_footer(); ?>
+
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -10,3 +10,12 @@ register_nav_menus(array(
 	'main' => __( 'Main Menu' ),
 	'footer' => __( 'Footer Menu' ),
 ));
+
+//add_theme_support( 'title-tag' );
+function mtTheme_wp_title($title){
+	if (is_page('mt')) {
+		// TODO
+	}
+	return $title.' | '.get_bloginfo('name').' - '.get_bloginfo('description');
+}
+add_filter('wp_title', 'mtTheme_wp_title');
