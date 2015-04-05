e5d4a28f988edb8ab689f8d7843eedc2ed0a4de3
Xennis <code@xennis.org>
2015-04-05 15:52:12 +0200

    Include tag gallery

--- page-mt.php
+++ page-mt.php
@@ -6,15 +6,21 @@
 			$id = intval(get_query_var('mtId'));
 			switch ($viewType) {
 				case 'bilder/galerie':
-					require_once(MT_DIR . '/public/view/Gallery.php');		
-					$view = new MT_View_Gallery($id, get_query_var('mtPage', 1), get_query_var('mtNum', 10), get_query_var('mtSort', 'date'));
+					require_once(MT_DIR.'/public/view/gallery/Gallery.php');
+					require_once(MT_DIR.'/public/view/gallery/StaticGallery.php');		
+					$view = new MT_View_StaticGallery($id, get_query_var('mtPage', 1), get_query_var('mtNum', 10), get_query_var('mtSort', 'date'));
 					break;
+				case 'bilder/tag':
+					require_once(MT_DIR.'/public/view/gallery/Gallery.php');
+					require_once(MT_DIR.'/public/view/gallery/TagGallery.php');		
+					$view = new MT_View_TagGallery(get_query_var('mtTag'));
+					break;				
 				case 'bilder/kategorie':
-					require_once(MT_DIR . '/public/view/Category.php');
+					require_once(MT_DIR.'/public/view/Category.php');
 					$view = new MT_View_Category($id);
 					break;
 				case 'fotograf':
-					require_once(MT_DIR . '/public/view/Photographer.php');		
+					require_once(MT_DIR.'/public/view/Photographer.php');		
 					$view = new MT_View_Photographer($id);
 					break;
 			}
