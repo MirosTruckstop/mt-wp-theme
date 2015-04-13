18d1e330b13a114da4bb9c50fab71d1010d26853
Xennis <code@xennis.org>
2015-04-13 21:35:12 +0200

    .

--- page-mt.php
+++ page-mt.php
@@ -1,42 +1,42 @@
 <?php
-	if (defined('MT_DIR')) {
-		try {
-			require_once(MT_DIR . '/public/view/ICommon.php');		
-			$viewType = get_query_var('mtView');
-			$id = intval(get_query_var('mtId'));
-			switch ($viewType) {
-				case 'bilder/galerie':
-					require_once(MT_DIR.'/public/view/gallery/Gallery.php');
-					require_once(MT_DIR.'/public/view/gallery/StaticGallery.php');		
-					$view = new MT_View_StaticGallery($id, get_query_var('mtPage', 1), get_query_var('mtNum', 10), get_query_var('mtSort', 'date'));
-					break;
-				case 'bilder/tag':
-					require_once(MT_DIR.'/public/view/gallery/Gallery.php');
-					require_once(MT_DIR.'/public/view/gallery/TagGallery.php');		
-					$view = new MT_View_TagGallery(get_query_var('mtTag'));
-					break;				
-				case 'bilder/kategorie':
-					require_once(MT_DIR.'/public/view/Category.php');
-					$view = new MT_View_Category($id);
-					break;
-				case 'fotograf':
-					require_once(MT_DIR.'/public/view/Photographer.php');		
-					$view = new MT_View_Photographer($id);
-					break;
-			}
-		} catch (Exception $e) {
-			require_once(MT_DIR . '/public/view/Error.php');
-			$view = new MT_View_Error($e->getMessage());
+if (defined('MT_DIR')) {
+	try {
+		require_once(MT_DIR . '/public/view/Common.php');		
+		$viewType = get_query_var('mtView');
+		$id = intval(get_query_var('mtId'));
+		switch ($viewType) {
+			case 'bilder/galerie':
+				require_once(MT_DIR.'/public/view/gallery/Gallery.php');
+				require_once(MT_DIR.'/public/view/gallery/StaticGallery.php');		
+				$view = new MT_View_StaticGallery($id, get_query_var('mtPage', 1), get_query_var('mtNum', 10), get_query_var('mtSort', 'date'));
+				break;
+			case 'bilder/tag':
+				require_once(MT_DIR.'/public/view/gallery/Gallery.php');
+				require_once(MT_DIR.'/public/view/gallery/TagGallery.php');		
+				$view = new MT_View_TagGallery(get_query_var('mtTag'));
+				break;				
+			case 'bilder/kategorie':
+				require_once(MT_DIR.'/public/view/Category.php');
+				$view = new MT_View_Category($id);
+				break;
+			case 'fotograf':
+				require_once(MT_DIR.'/public/view/Photographer.php');		
+				$view = new MT_View_Photographer($id);
+				break;
 		}
-	} else {
-		echo 'Constant MT_DIR is undefined.';
+	} catch (Exception $e) {
+		require_once(MT_DIR . '/public/view/Error.php');
+		$view = new MT_View_Error($e->getMessage());
 	}
+} else {
+	echo 'Constant MT_DIR is undefined.';
+}
 ?>
 
 <?php get_header(); ?>
 	<article>
-		<?php (method_exists($view, outputBreadcrumb) ? $view->outputBreadcrumb() : ''); ?>
-		<h2><?php $view->outputTitle(); ?></h2>
+		<?php $view->outputBreadcrumb(); ?>
+		<h1><?php $view->outputTitle(); ?></h1>
 		<?php $view->outputContent(); ?>
 	</article>
 <?php get_footer(); ?>
