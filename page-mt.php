49814cf1eb4243d484844d73e7498f5f3ab49daa
Xennis <code@xennis.org>
2015-06-17 18:21:55 +0200

    Change paths

--- page-mt.php
+++ page-mt.php
@@ -1,25 +1,25 @@
 <?php
 if (defined('MT_DIR')) {
-	require_once(MT_DIR . '/public/view/Common.php');		
+	require_once(MT_DIR . '/src/public/view/Common.php');		
 	$viewType = get_query_var('mtView');
 	$id = intval(get_query_var('mtId'));
 	switch ($viewType) {
 		case 'bilder/galerie':
-			require_once(MT_DIR.'/public/view/gallery/Gallery.php');
-			require_once(MT_DIR.'/public/view/gallery/StaticGallery.php');		
+			require_once(MT_DIR.'/src/public/view/gallery/Gallery.php');
+			require_once(MT_DIR.'/src/public/view/gallery/StaticGallery.php');		
 			$view = new MT_View_StaticGallery($id, get_query_var('mtPage', 1), get_query_var('mtNum', 10), get_query_var('mtSort', 'date'));
 			break;
 		case 'bilder/tag':
-			require_once(MT_DIR.'/public/view/gallery/Gallery.php');
-			require_once(MT_DIR.'/public/view/gallery/TagGallery.php');		
+			require_once(MT_DIR.'/src/public/view/gallery/Gallery.php');
+			require_once(MT_DIR.'/src/public/view/gallery/TagGallery.php');		
 			$view = new MT_View_TagGallery(get_query_var('mtTag'));
 			break;				
 		case 'bilder/kategorie':
-			require_once(MT_DIR.'/public/view/Category.php');
+			require_once(MT_DIR.'/src/public/view/Category.php');
 			$view = new MT_View_Category($id);
 			break;
 		case 'fotograf':
-			require_once(MT_DIR.'/public/view/Photographer.php');		
+			require_once(MT_DIR.'/src/public/view/Photographer.php');		
 			$view = new MT_View_Photographer($id);
 			break;
 		default:
